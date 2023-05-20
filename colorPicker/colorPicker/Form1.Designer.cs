e1cd91d28bdb83f4dc30e0bda0b439580157987c
boraaziz <boraazizozcan@hotmail.com>
2023-05-21 01:28:21 +0300

    FUN
    Fun project.

---
+++ colorPicker/colorPicker/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace colorPicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.palet = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar3 = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKopyala = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.kontrol = new System.Windows.Forms.Timer(this.components);
+            this.timerKopya = new System.Windows.Forms.Timer(this.components);
+            this.cdcBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // palet
+            // 
+            this.palet.ButtonSpecs.FormClose.Image = global::colorPicker.Properties.Resources.mc_red;
+            this.palet.ButtonSpecs.FormMax.Image = global::colorPicker.Properties.Resources.mc_yellw;
+            this.palet.ButtonSpecs.FormMin.Image = global::colorPicker.Properties.Resources.mc_green;
+            this.palet.ButtonStyles.ButtonForm.StateNormal.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.ButtonStyles.ButtonForm.StateNormal.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.ButtonStyles.ButtonForm.StateNormal.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.palet.ButtonStyles.ButtonForm.StateNormal.Border.Width = 0;
+            this.palet.ButtonStyles.ButtonForm.StatePressed.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.ButtonStyles.ButtonForm.StatePressed.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.ButtonStyles.ButtonForm.StatePressed.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.palet.ButtonStyles.ButtonForm.StatePressed.Border.Width = 0;
+            this.palet.ButtonStyles.ButtonForm.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.ButtonStyles.ButtonForm.StateTracking.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.ButtonStyles.ButtonForm.StateTracking.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.palet.ButtonStyles.ButtonForm.StateTracking.Border.Width = 0;
+            this.palet.FormStyles.FormMain.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.FormStyles.FormMain.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.FormStyles.FormMain.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.FormStyles.FormMain.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.FormStyles.FormMain.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.palet.FormStyles.FormMain.StateCommon.Border.GraphicsHint = ComponentFactory.Krypton.Toolkit.PaletteGraphicsHint.None;
+            this.palet.FormStyles.FormMain.StateCommon.Border.Rounding = 12;
+            this.palet.HeaderStyles.HeaderForm.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.HeaderStyles.HeaderForm.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.HeaderStyles.HeaderForm.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.HeaderStyles.HeaderForm.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.palet.HeaderStyles.HeaderForm.StateCommon.Border.DrawBorders = ((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) 
+            | ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right)));
+            this.palet.HeaderStyles.HeaderForm.StateCommon.ButtonEdgeInset = 10;
+            this.palet.HeaderStyles.HeaderForm.StateCommon.Content.Padding = new System.Windows.Forms.Padding(10, -1, -1, -1);
+            this.palet.PalettePaint += new System.EventHandler<ComponentFactory.Krypton.Toolkit.PaletteLayoutEventArgs>(this.kryptonPalette1_PalettePaint);
+            // 
+            // vScrollBar1
+            // 
+            resources.ApplyResources(this.vScrollBar1, "vScrollBar1");
+            this.vScrollBar1.Maximum = 264;
+            this.vScrollBar1.Name = "vScrollBar1";
+            // 
+            // vScrollBar2
+            // 
+            resources.ApplyResources(this.vScrollBar2, "vScrollBar2");
+            this.vScrollBar2.Maximum = 264;
+            this.vScrollBar2.Name = "vScrollBar2";
+            // 
+            // vScrollBar3
+            // 
+            resources.ApplyResources(this.vScrollBar3, "vScrollBar3");
+            this.vScrollBar3.Maximum = 264;
+            this.vScrollBar3.Name = "vScrollBar3";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Name = "label1";
+            // 
+            // btnKopyala
+            // 
+            this.btnKopyala.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnKopyala.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.btnKopyala, "btnKopyala");
+            this.btnKopyala.Name = "btnKopyala";
+            this.btnKopyala.UseVisualStyleBackColor = false;
+            this.btnKopyala.Click += new System.EventHandler(this.btnKopyala_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // kontrol
+            // 
+            this.kontrol.Enabled = true;
+            this.kontrol.Interval = 1;
+            this.kontrol.Tick += new System.EventHandler(this.kontrol_Tick);
+            // 
+            // timerKopya
+            // 
+            this.timerKopya.Interval = 1000;
+            this.timerKopya.Tick += new System.EventHandler(this.timerKopya_Tick);
+            // 
+            // cdcBox
+            // 
+            resources.ApplyResources(this.cdcBox, "cdcBox");
+            this.cdcBox.Name = "cdcBox";
+            this.cdcBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.Controls.Add(this.cdcBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnKopyala);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vScrollBar3);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Palette = this.palet;
+            this.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Custom;
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public ComponentFactory.Krypton.Toolkit.KryptonPalette palet;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.VScrollBar vScrollBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKopyala;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer kontrol;
+        private System.Windows.Forms.Timer timerKopya;
+        private System.Windows.Forms.CheckBox cdcBox;
+
+    }
+}
+
---
+++ colorPicker/colorPicker/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace colorPicker
+{
+    public partial class Form1 : KryptonForm
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kryptonPalette1_PalettePaint(object sender, ComponentFactory.Krypton.Toolkit.PaletteLayoutEventArgs e)
+        {
+            this.MaximizeBox = false;
+        }
+        int r, g, b;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vScrollBar1.Value = 250;
+            vScrollBar2.Value = 252;
+            vScrollBar3.Value = 252;
+        }
+
+        private void kontrol_Tick(object sender, EventArgs e)
+        {   
+            r = vScrollBar1.Value;
+            g = vScrollBar2.Value;
+            b = vScrollBar3.Value;
+            Color darkBg = ControlPaint.Dark(Color.FromArgb(r, g, b));
+
+            if (cdcBox.Checked == true)
+            {
+                this.BackColor = darkBg;
+                label1.ForeColor = Color.FromArgb(250,252,252);
+                cdcBox.ForeColor = Color.FromArgb(250, 252, 252);
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(250,252,252);
+                label1.ForeColor = Color.Black;
+                cdcBox.ForeColor = Color.Black;
+            }
+
+            textBox1.BackColor = Color.FromArgb(r, g, b);
+            label1.Text = "rgb(" + r + "," + g + "," + b + ")";
+
+            //palet.FormStyles.FormCommon.StateCommon.Back.Color1 = darkBg;
+            //palet.FormStyles.FormCommon.StateCommon.Back.Color2 = darkBg;
+            //palet.FormStyles.FormCommon.StateCommon.Border.Color1 = darkBg;
+            //palet.FormStyles.FormCommon.StateCommon.Border.Color2 = darkBg;
+            //palet.HeaderStyles.HeaderForm.StateCommon.Back.Color1 = darkBg;
+            //palet.HeaderStyles.HeaderForm.StateCommon.Back.Color2 = darkBg;
+            //palet.HeaderStyles.HeaderForm.StateCommon.Border.Color1 = darkBg;
+            //palet.HeaderStyles.HeaderForm.StateCommon.Border.Color2 = darkBg;
+        }
+
+        private void btnKopyala_Click(object sender, EventArgs e)
+        {
+            btnKopyala.Text = "Kopyalandı!";
+            timerKopya.Enabled = true;
+            timerKopya.Start();
+            Clipboard.SetText(label1.Text);
+        }
+
+        private void timerKopya_Tick(object sender, EventArgs e)
+        {
+            for (int sayac = 0; sayac <= 7; sayac++)
+            {
+                btnKopyala.Text = "Kopyala!";
+            }
+
+            timerKopya.Stop();
+        }
+
+    }
+}
